fdd30bdb164f705e52bba058bd1e69cda3a00db6
zied <zied.hack@gmail.com>
2019-12-02 04:31:52 +0100

    login and loguot

--- app/process_login.php
+++ app/process_login.php
@@ -1,11 +1,5 @@
 <?php
 
-// this script acts like a controller
-// to process a login a JS request from form-submission.js is sent to this script
-// view -> js -> process_login -> server
-// view <- js <- process_login <- server
-// other process scripts follow the same cycle
-
 ob_start();
 session_start();
 
@@ -31,9 +25,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["submitBtn"])) {
 
       $sql = "SELECT * FROM `client` WHERE email = '$email' and password = '".md5($password)."' ";
        $result = mysqli_query($con,$sql);
-       if($result){
+        $rows1 = mysqli_num_rows($result);
+
+       if($rows1 == 1){
          $sql2 = "SELECT * FROM `client` WHERE email='$email' and isadmin = 1 ";
           $result2 = mysqli_query($con,$sql2);
+            $rows2 = mysqli_num_rows($result2);
+            
           if($result2){
             $_SESSION["username"] = $_POST["email"];
             $_SESSION["accountEmail"] = $_POST["email"];
@@ -49,50 +47,5 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["submitBtn"])) {
        }else {
          echo Util::displayAlertV1("Incorrect password or email ", "warning");
        }
-
-/*
-        $adminHandler = new AdminHandler();
-        $admin = new Admin();
-        // before running this method, make sure email exists
-        $admin->setEmail($_POST["email"]);
-        $adminId = ($adminHandler->getObjectUtil($admin->getEmail())->getAdminId());
-
-        if ($adminId > 1 || intval($adminId) > 0) {
-            $_SESSION["username"] = $_POST["email"];
-            $_SESSION["accountEmail"] = $_POST["email"];
-            $_SESSION['isAdmin'] = 1;
-            echo $_SESSION['isAdmin'];
-        } else {
-            $handler = new CustomerHandler();
-            $customer = new Customer();
-            $customer->setEmail($_POST["email"]);
-
-            $newCustomer = new Customer();
-            if (!$handler->isPasswordMatchWithEmail($_POST['password'], $customer)) {
-                echo Util::displayAlertV1("Incorrect password.", "warning");
-            } else {
-                $_SESSION["username"] = $handler->getUsername($_POST["email"]);
-                $_SESSION["accountEmail"] = $customer->getEmail();
-                $_SESSION["authenticated"] = 1;
-                $_SESSION["password"] = $_POST["password"];
-
-                // set the session phone number too
-                if ($handler->getCustomerObj($_POST["email"])->getPhone()) {
-                    $_SESSION["phoneNumber"] = $handler->getCustomerObj($_POST["email"])->getPhone();
-                }
-                echo $_SESSION["authenticated"];
-            }
-        }
-        */
     }
 }
-
-/**
- * [x] validate the fields first
- * [x] if no errors check if email is registered
- *     if not registered, display not registered message
- *     otherwise, create a customer object
- * [x] check if password entered match with db password
- *     if not match display incorrect message
- *     otherwise, create a session variables
- */
--- app/process_logout.php
+++ app/process_logout.php
@@ -1,13 +1,9 @@
 <?php
-
 session_start();
-
 if ($_SERVER["REQUEST_METHOD"] == "GET") {
     // Unset all of the session variables.
     $_SESSION = array();
 
-    // If it's desired to kill the session, also delete the session cookie.
-    // Note: This will destroy the session, and not just the session data!
     if (ini_get("session.use_cookies")) {
         $params = session_get_cookie_params();
         setcookie(
@@ -20,8 +16,6 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
             $params["httponly"]
         );
     }
-
-    // Finally, destroy the session.
     session_destroy();
     $sessionsDeleted = 1;
     echo $sessionsDeleted;
